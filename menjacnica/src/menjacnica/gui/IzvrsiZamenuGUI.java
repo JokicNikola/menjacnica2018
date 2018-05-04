9015629124b18553df4182b339b072aafb457408
JokicNikola <jokic1916@fon.bg.ac.rs>
2018-05-04 05:09:39 +0200

    Napravljene SO, Kontroler i ispravljeni prozori

--- menjacnica/src/menjacnica/gui/IzvrsiZamenuGUI.java
+++ menjacnica/src/menjacnica/gui/IzvrsiZamenuGUI.java
@@ -27,6 +27,8 @@ import java.awt.event.ActionEvent;
 import javax.swing.ButtonGroup;
 
 import menjacnica.Valuta;
+import menjacnica.gui.kontoler.GuiKontroler;
+
 import java.awt.Toolkit;
 
 public class IzvrsiZamenuGUI extends JFrame {
@@ -48,7 +50,7 @@ public class IzvrsiZamenuGUI extends JFrame {
 	private final ButtonGroup buttonGroup = new ButtonGroup();
 	private JTextField textFieldValuta;
 	private JLabel lblKonacniIznos;
-	private JTextField textFieldKonacniIznos;
+	public static JTextField textFieldKonacniIznos;
 
 	private MenjacnicaGUI glavniProzor;
 	private Valuta valuta;
@@ -56,7 +58,7 @@ public class IzvrsiZamenuGUI extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public IzvrsiZamenuGUI(MenjacnicaGUI glavniProzor, Valuta valuta) {
+	public IzvrsiZamenuGUI( Valuta valuta) {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(IzvrsiZamenuGUI.class.getResource("/icons/Screenshot.png")));
 		setTitle("Izvrsi zamenu");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -83,7 +85,7 @@ public class IzvrsiZamenuGUI extends JFrame {
 		contentPane.add(getTextFieldKonacniIznos());
 		
 		//podesavanje
-		this.glavniProzor = glavniProzor;
+		
 		this.valuta = valuta;
 				
 		prikaziValutu();
@@ -164,7 +166,7 @@ public class IzvrsiZamenuGUI extends JFrame {
 			btnIzvrsiZamenu = new JButton("Izracunaj iznos");
 			btnIzvrsiZamenu.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
-					izvrsiZamenu();
+					GuiKontroler.izvrsiZamenu(valuta,!rdbtnProdaja.isSelected(), Double.parseDouble(textFieldIznos.getText()));
 				}
 			});
 			btnIzvrsiZamenu.setBounds(24, 234, 160, 25);
@@ -239,17 +241,5 @@ public class IzvrsiZamenuGUI extends JFrame {
 		textFieldValuta.setText(valuta.getSkraceniNaziv());
 	}
 	
-	private void izvrsiZamenu(){
-		try{
-			double konacniIznos = 
-					glavniProzor.sistem.izvrsiTransakciju(valuta,
-							rdbtnProdaja.isSelected(), 
-							Double.parseDouble(textFieldIznos.getText()));
-		
-			textFieldKonacniIznos.setText(""+konacniIznos);
-		} catch (Exception e1) {
-		JOptionPane.showMessageDialog(contentPane, e1.getMessage(),
-				"Greska", JOptionPane.ERROR_MESSAGE);
-	}
-	}
+	
 }
---
+++ menjacnica/src/menjacnica/gui/kontoler/GuiKontroler.java
@@ -0,0 +1,139 @@
+package menjacnica.gui.kontoler;
+
+import java.awt.EventQueue;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+import menjacnica.Menjacnica;
+import menjacnica.MenjacnicaInterface;
+import menjacnica.Valuta;
+import menjacnica.gui.DodajKursGUI;
+import menjacnica.gui.IzvrsiZamenuGUI;
+import menjacnica.gui.MenjacnicaGUI;
+import menjacnica.gui.ObrisiKursGUI;
+import menjacnica.gui.models.MenjacnicaTableModel;
+
+public class GuiKontroler {
+
+	public static MenjacnicaInterface menjacnica = new Menjacnica();
+	public static MenjacnicaGUI gp;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenjacnicaGUI frame = new MenjacnicaGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public static void sacuvajUFajl() {
+		try {
+			JFileChooser fc = new JFileChooser();
+			int returnVal = fc.showSaveDialog(null);
+
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				File file = fc.getSelectedFile();
+
+				menjacnica.sacuvajUFajl(file.getAbsolutePath());
+			}
+		} catch (Exception e1) {
+			JOptionPane.showMessageDialog(gp, e1.getMessage(),
+					"Greska", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	
+	public static void ugasiAplikaciju() {
+		int opcija = JOptionPane.showConfirmDialog(null,
+				"Da li ZAISTA zelite da izadjete iz apliacije", "Izlazak",
+				JOptionPane.YES_NO_OPTION);
+
+		if (opcija == JOptionPane.YES_OPTION)
+			System.exit(0);
+	}
+	
+	public static void ucitajIzFajla() {
+		try {
+			JFileChooser fc = new JFileChooser();
+			int returnVal = fc.showOpenDialog(null);
+
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				File file = fc.getSelectedFile();
+				menjacnica.ucitajIzFajla(file.getAbsolutePath());
+				gp.prikaziSveValute();
+			}	
+		} catch (Exception e1) {
+			JOptionPane.showMessageDialog(null, e1.getMessage(),
+					"Greska", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	
+	public static void prikaziAboutProzor(){
+		JOptionPane.showMessageDialog(null,
+				"Autor: Bojan Tomic, Verzija 1.0", "O programu Menjacnica",
+				JOptionPane.INFORMATION_MESSAGE);
+	}
+	
+	public static void prikaziDodajKursGUI() {
+		DodajKursGUI prozor = new DodajKursGUI();
+		prozor.setLocationRelativeTo(null);
+		prozor.setVisible(true);
+	}
+	
+public static void prikaziObrisiKursGUI() {
+		
+		if (MenjacnicaGUI.table.getSelectedRow() != -1) {
+			MenjacnicaTableModel model = (MenjacnicaTableModel)(MenjacnicaGUI.table.getModel());
+			ObrisiKursGUI prozor = new ObrisiKursGUI(
+					model.vratiValutu(MenjacnicaGUI.table.getSelectedRow()));
+			prozor.setLocationRelativeTo(gp);
+			prozor.setVisible(true);
+		}
+	}
+
+public static void prikaziIzvrsiZamenuGUI() {
+	if (MenjacnicaGUI.table.getSelectedRow() != -1) {
+		MenjacnicaTableModel model = (MenjacnicaTableModel)(MenjacnicaGUI.table.getModel());
+		IzvrsiZamenuGUI prozor = new IzvrsiZamenuGUI(
+				model.vratiValutu(MenjacnicaGUI.table.getSelectedRow()));
+		prozor.setLocationRelativeTo(gp);
+		prozor.setVisible(true);
+	}
+}
+
+public static void obrisiValutu(Valuta valuta) {
+	try{
+		menjacnica.obrisiValutu(valuta);
+
+		
+		gp.prikaziSveValute();
+		
+	} catch (Exception e1) {
+		JOptionPane.showMessageDialog(null, e1.getMessage(),
+				"Greska", JOptionPane.ERROR_MESSAGE);
+	}
+}
+
+public static void izvrsiZamenu(Valuta valuta, boolean prodaja, double iznos){
+	try{
+		double konacniIznos = 
+				menjacnica.izvrsiTransakciju(valuta, prodaja, iznos);
+	
+		IzvrsiZamenuGUI.textFieldKonacniIznos.setText(""+konacniIznos);
+	} catch (Exception e1) {
+	JOptionPane.showMessageDialog(gp, e1.getMessage(),
+			"Greska", JOptionPane.ERROR_MESSAGE);
+}
+}
+	
+	
+}
